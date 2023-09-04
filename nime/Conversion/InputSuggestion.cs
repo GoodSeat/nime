948a0a5a05ab10bf54a76547b991135e4290ee33
GoodSeat <GoodSeat@hotmail.co.jp>
2023-09-05 00:10:16 +0900

    備忘録更新。

--- nime/Conversion/InputSuggestion.cs
+++ nime/Conversion/InputSuggestion.cs
@@ -8,5 +8,33 @@ namespace GoodSeat.Nime.Conversion
 {
     internal class InputSuggestion
     {
+
+        public Task RegisterHiraganaSequenceAsync(ConvertCandidate convertCandidate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HiraganaSequenceTree> SearchStartWithAsync(string hiragana)
+        {
+            throw new NotImplementedException();
+        }
+
+        // ひらがな2文字をキーとした、日本語文節とそれに続く文節リストの対応マップ
+        Dictionary<string, Dictionary<HiraganaSet, List<HiraganaSet>>> _mapHistoryHiraganaSequence;
+
     }
+
+    internal record HiraganaSet(string Hiragana, string Phrase);
+
+    internal class HiraganaSequenceTree
+    {
+
+        public List<List<HiraganaSet>> Take(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+
 }
